017954dea22bdd589cbb0f4569732e4d8171e728
Pieter Jordaan <pieter.jordaan@way2web.nl>
2020-07-14 16:16:59 +0200

    remove conflict phpspec/prophecy by fixing the test

--- tests/Plugins/Mock/ResourceFactories/MockApiResourceFactoryTest.php
+++ tests/Plugins/Mock/ResourceFactories/MockApiResourceFactoryTest.php
@@ -2,9 +2,11 @@
 namespace W2w\Test\Apie\Plugins\Mock\ResourceFactories;
 
 use PHPUnit\Framework\TestCase;
+use W2w\Lib\Apie\Core\SearchFilters\SearchFilterRequest;
 use W2w\Lib\Apie\Interfaces\ApiResourceFactoryInterface;
 use W2w\Lib\Apie\Interfaces\ApiResourcePersisterInterface;
 use W2w\Lib\Apie\Interfaces\ApiResourceRetrieverInterface;
+use W2w\Lib\Apie\Plugins\Core\DataLayers\MemoryDataLayer;
 use W2w\Lib\Apie\Plugins\Mock\DataLayers\MockApiResourceDataLayer;
 use W2w\Lib\Apie\Plugins\Mock\ResourceFactories\MockApiResourceFactory;
 
@@ -22,8 +24,35 @@ class MockApiResourceFactoryTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->skippedRetriever = $this->prophesize(ApiResourceRetrieverInterface::class)->reveal();
-        $this->skippedPersister = $this->prophesize(ApiResourcePersisterInterface::class)->reveal();
+        $this->skippedRetriever = new class implements ApiResourceRetrieverInterface
+        {
+            public function retrieve(string $resourceClass, $id, array $context)
+            {
+                // TODO: Implement retrieve() method.
+            }
+
+            public function retrieveAll(string $resourceClass, array $context, SearchFilterRequest $searchFilterRequest
+            ): iterable {
+                // TODO: Implement retrieveAll() method.
+            }
+        };
+        $this->skippedPersister = new class implements ApiResourcePersisterInterface
+        {
+            public function persistNew($resource, array $context = [])
+            {
+                // TODO: Implement persistNew() method.
+            }
+
+            public function persistExisting($resource, $int, array $context = [])
+            {
+                // TODO: Implement persistExisting() method.
+            }
+
+            public function remove(string $resourceClass, $id, array $context)
+            {
+                // TODO: Implement remove() method.
+            }
+        };
 
         $this->retriever = $this->prophesize(MockApiResourceDataLayer::class);
         $this->factory = $this->prophesize(ApiResourceFactoryInterface::class);
@@ -39,8 +68,8 @@ class MockApiResourceFactoryTest extends TestCase
         $retriever = $this->prophesize(ApiResourceRetrieverInterface::class)->reveal();
         $this->factory->getApiResourceRetrieverInstance('donut-retriever')->shouldBeCalled()->willReturn($retriever);
 
-        $this->assertEquals(
-            $this->retriever->reveal(),
+        $this->assertInstanceOf(
+            MockApiResourceDataLayer::class,
             $this->testItem->getApiResourceRetrieverInstance('donut-retriever')
         );
     }
@@ -60,8 +89,8 @@ class MockApiResourceFactoryTest extends TestCase
         $persister = $this->prophesize(ApiResourcePersisterInterface::class)->reveal();
         $this->factory->getApiResourcePersisterInstance('donut-retriever')->shouldBeCalled()->willReturn($persister);
 
-        $this->assertEquals(
-            $this->retriever->reveal(),
+        $this->assertInstanceOf(
+            MockApiResourceDataLayer::class,
             $this->testItem->getApiResourcePersisterInstance('donut-retriever')
         );
     }
